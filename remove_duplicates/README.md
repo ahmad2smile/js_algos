3235d13cb0fce70ec65c12c4631e5b9905ddb760
ahmad <ahmad2smile@gmail.com>
2017-04-14 01:13:12 +0500

    Added Remove Dublicates in Array algo

---
+++ remove_duplicates/README.md
@@ -0,0 +1,11 @@
+
+#   Remove duplicates:
+
+####    Function    `removeDub()`
+
+Takes in an Array. Check for the occurrence of Elements in the array and pushes it to the `clearedArray` if its not in there already.
+
+
+####    Output  Array `clearedArray`
+
+Array contains the Elements of the Array without any duplicates
---
+++ remove_duplicates/remove_duplicates.js
@@ -0,0 +1,22 @@
+
+const inputArray = [ 0, 1, 1, 1, 2, 3, 2, 4];
+
+function removeDub(inarr) {
+    let clearedArray = [];
+
+    inarr.forEach((x) =>{
+        if (!clearedArray.includes(x)) clearedArray.push(x);
+    });
+
+    return clearedArray;
+}
+
+///////////////////////////////////
+//Display Jorgan
+
+process.stdout.write('\033c');
+console.log(inputArray);
+console.log(removeDub(inputArray));
+
+//Display Jorgan
+///////////////////////////////////
