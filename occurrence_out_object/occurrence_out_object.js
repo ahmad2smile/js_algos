1b7a49399065cafe3f8e65ddcfde65b330c5e492
Shafiq Ahmad <ahmad2smile@gmail.com>
2017-04-18 10:41:23 +0500

    Removed unused if check

--- occurrence_out_object/occurrence_out_object.js
+++ occurrence_out_object/occurrence_out_object.js
@@ -5,9 +5,7 @@ const inputArray = [ 0, 1, 1, 1, 2, 3, 2, 4];
 function countOccurance(inarr) {
     let occuranceOf = {};
     inarr.forEach((x) =>{
-        if (inarr.includes(x)) {
-            occuranceOf[x] ? ++occuranceOf[x] : occuranceOf[x] = 1;
-        }
+        occuranceOf[x] ? ++occuranceOf[x] : occuranceOf[x] = 1;
     });
     return occuranceOf;
 }
